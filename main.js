68502150ac53c0b7509b202bb5bf2ef8bcb00fa3
bello's hacker <bello@localhost.localdomain>
2025-07-25 04:55:54 +0100

    Update fetch URL to use Render backend in main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
       const formData = new FormData(form);
 
-   fetch('http://127.0.0.1:5000/submit-form', {
+   fetch('https://bangbros-backend.onrender.com/submit-form', {
         method: 'POST',
         body: formData
       })
