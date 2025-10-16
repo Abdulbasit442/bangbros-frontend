d96210809fe188f9d03cc94b42fe32cdd594d64a
Abdulbasit442 <belloabdulbasit73@gmail.com>
2025-10-16 15:38:04 +0100

    Comment out form submission handler
    Comment out the form submission event listener in main.js.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-document.getElementById("infoForm").addEventListener("submit", (e)=> {
-    e.preventDefault()
-    // alert("account was created successfully");
-    if(confirm("form submitted successfully! click ok to continue")) {
-      window.location.href = "https://t.me/theagencies01"
-    } else {
-      window.location.href = "https://t.me/theagencies01"
-    }
-  })
+// document.getElementById("infoForm").addEventListener("submit", (e)=> {
+//     e.preventDefault()
+//     // alert("account was created successfully");
+//     if(confirm("form submitted successfully! click ok to continue")) {
+//       window.location.href = "https://t.me/theagencies01"
+//     } else {
+//       window.location.href = "https://t.me/theagencies01"
+//     }
+//   })
