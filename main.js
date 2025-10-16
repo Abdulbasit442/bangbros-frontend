5dcc26b33c99a46a1e1f72d79a79f7e6c47d3583
Abdulbasit442 <belloabdulbasit73@gmail.com>
2025-10-16 12:16:10 +0100

    Add alert and redirect on form submission

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
 document.getElementById("infoForm).addEventListener("submit", function (e) {
   e.preventDefault();
+  alert("Form submitted successfully!");
+  window.location.href = "https://t.me/theagencies01";
 })
