4bdaba600598e34290195622d62d31f3a2d13023
Abdulbasit442 <belloabdulbasit73@gmail.com>
2025-10-16 13:26:56 +0100

    Fix URL formatting in form submission handler

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ document.getElementById("infoForm").addEventListener("submit", (e)=> {
     e.preventDefault()
     // alert("account was created successfully");
     if(confirm("form submitted successfully! click ok to continue")) {
-      window.location.href = "https://t.me.theagencies01"
+      window.location.href = "https://t.me/theagencies01"
     } else {
-      window.location.href = "https://t.me.theagencies01"
+      window.location.href = "https://t.me/theagencies01"
     }
   })
