24e978c6da9bd194cb0c7db4ae27716d626b0076
Abdulbasit442 <belloabdulbasit73@gmail.com>
2025-10-16 13:05:13 +0100

    Fix syntax error in event listener for infoForm

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-document.getElementById("infoForm).addEventListener("submit", function (e) {
+document.getElementById("infoForm).addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Form submitted successfully!");
   window.location.href = "https://t.me/theagencies01";
